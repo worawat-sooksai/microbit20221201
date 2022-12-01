e398abe10698351d4ba1561a017fc96d60e8f88a
worawat-sooksai <75353454+worawat-sooksai@users.noreply.github.com>
2022-12-01 16:28:16 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("What your name ?")
+    radio.sendString("A")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
+    if ("A" == receivedString) {
+        serial.writeLine(receivedString)
+        basic.showString(receivedString)
+    }
+    if ("B" == receivedString) {
+        serial.writeLine(receivedString)
+        basic.showString(receivedString)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendString("My name is KRITSADA")
+    radio.sendString("B")
 })
-radio.setGroup(108)
+radio.setGroup(102)
+basic.forever(function () {
+    serial.writeLine("X:" + input.acceleration(Dimension.X))
+    serial.writeLine("Y:" + input.acceleration(Dimension.Y))
+    serial.writeLine("Z:" + input.acceleration(Dimension.Z))
+})
